19171a840dfdb4bbcac7fc108b9b60dfd70f5487
Kenneth Verstraete <kennethverstraete@hotmail.com>
2013-12-12 03:03:33 +0100

    werkend

--- Bachelorproef/src/Gurobi/TUP.java
+++ Bachelorproef/src/Gurobi/TUP.java
@@ -1,6 +1,7 @@
 package Gurobi;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 import datareader.Datareader;
 
@@ -13,21 +14,41 @@ public class TUP {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
+		
+		/////////////////
+		//Kies dataset //
+		/////////////////
+		String dataset = "4"; // Opl: 5176
+//		String dataset = "6"; // Opl: 14077
+//		String dataset = "6a"; // Opl: 15457
+//		String dataset = "6b"; // Opl: 16716
+//		String dataset = "6c"; // Opl: 14396
+//		String dataset = "8"; // Opl: 34311
+//		String dataset = "8a"; // Opl: 31490  
+//		String dataset = "8b"; // Opl: 32731 
+//		String dataset = "8c"; // Opl: 29879 
+//		String dataset = "10"; // Opl: 48942
+//		String dataset = "10a"; // Opl: 46551
+//		String dataset = "10b"; // Opl: 45609
+//		String dataset = "10c"; // Opl: 43149
+
+		execute(dataset);
+		
+		// Test allemaal
+//		String[] datasets = {"4", "6", "6a", "6b", "6c", "8", "8a"};//, "8b", "8c", "10", "10a", "10b", "10c"};
+//		for(String s: datasets) {
+//			execute(s);
+//			for(int i=0; i<100; i++) {
+//				System.out.print("-");
+//			}
+//			System.out.println();
+//		}
+	}
+	
+	public static void execute(String dataset) throws IOException {
 		try {		
-			// ----------------------------------
-			// ---------- KIES DATASET ----------
-			// ----------------------------------
-//			String dataset = "4"; // Opl: 5176
-//			String dataset = "6"; // Opl: 14077
-//			String dataset = "6a"; // Opl: 15457
-//			String dataset = "6b"; // Opl: 16716
-//			String dataset = "6c"; // Opl: 14396
-//			String dataset = "8"; // Opl: 34311
-//			String dataset = "8a"; // Opl: 31490  
-//			String dataset = "8b"; // Opl: 32731 
-//			String dataset = "8c"; // Opl: 29879 
-//			String dataset = "10"; // Opl: 48942
-			
+					
+			// Data inlezen
 			Datareader dr = new Datareader();
 			dr.getData("D:\\Dropbox\\School\\bachelorproef\\datasets\\umps"+dataset+".txt");
 			int[][] dist = dr.getDist();
@@ -35,6 +56,7 @@ public class TUP {
 			double n = dist.length/2;
 			double amountTeams = dist[0].length;
 			double amountSlots = 2*amountTeams-2;
+			System.out.println("number of teams = "+amountTeams);
 						
 			// Parameters
 			double d1 = 0;
@@ -53,7 +75,7 @@ public class TUP {
 				for(int u=0; u<n; ++u) {
 					for(int s=0; s<amountSlots; ++s) {
 						x[i][s][u] = 
-								model.addVar(0, 1, 1,GRB.BINARY, "x"+(i+1)+(s+1)+(u+1));
+								model.addVar(0, 1, 0,GRB.BINARY, "x"+(i+1)+(s+1)+(u+1));
 					}
 				}
 			}
@@ -74,20 +96,25 @@ public class TUP {
 			model.update();
 			
 			// Set objective:
-			GRBLinExpr expr = new GRBLinExpr();
-			for(int i=0; i<amountTeams; ++i) {
-				for(int j=0; j<amountTeams; ++j) {
-					for(int u=0; u<n; ++u) {
-						for(int s=0; s<amountSlots-1; ++s) {
-							expr.addTerm(dist[i][j],z[i][j][s][u]);
-						}
-					}
-				}
-			}
-			model.setObjective(expr, GRB.MINIMIZE);
+//			GRBLinExpr expr = new GRBLinExpr();
+//			for(int i=0; i<amountTeams; ++i) {
+//				for(int j=0; j<amountTeams; ++j) {
+//					for(int u=0; u<n; ++u) {
+//						for(int s=0; s<amountSlots-1; ++s) {
+//							expr.addTerm(dist[i][j],z[i][j][s][u]);
+//						}
+//					}
+//				}
+//			}
+//			model.setObjective(expr, GRB.MINIMIZE);
+			 
+			model.set(GRB.IntAttr.ModelSense, 1);
 									
-			// Constraints
-			// 2 (works)
+			/*
+			 * Constraints
+			 */
+			
+			// Constraint 2 (works)
 			for(int i=0; i<amountTeams;++i){
 				for(int s=0; s<amountSlots; ++s) {
 					if(opp[s][i] > 0 ) {
@@ -95,12 +122,12 @@ public class TUP {
 						for(int u=0; u<n; ++u) {
 								d1tot.addTerm(1.0,x[i][s][u]);
 						}
-						model.addConstr(d1tot, GRB.EQUAL, 1, "B1_x"+i+s);
+						model.addConstr(d1tot, GRB.EQUAL, 1, "C2_x"+i+s);
 					}
 				}
 			}
 			
-			// 3 (works)
+			// Constraint 3 (works)
 			for(int s=0; s<amountSlots;++s){
 				for(int u=0; u<n; ++u) {
 					GRBLinExpr d2tot = new GRBLinExpr();
@@ -109,11 +136,11 @@ public class TUP {
 							d2tot.addTerm(1.0,x[i][s][u]);
 						}
 					}
-					model.addConstr(d2tot, GRB.EQUAL, 1, "B2_x"+s+u);
+					model.addConstr(d2tot, GRB.EQUAL, 1, "C3_x"+s+u);
 				}
 			}
 			
-			// 4 (works)
+			// Constraint 4 (works)
 			for(int i=0; i<amountTeams;++i){
 				for(int u=0; u<n; ++u) {
 					GRBLinExpr d3tot = new GRBLinExpr();
@@ -122,26 +149,26 @@ public class TUP {
 							d3tot.addTerm(1.0,x[i][s][u]);
 						}
 					}
-					model.addConstr(d3tot, GRB.GREATER_EQUAL, 1, "B3_x"+i+u);
+					model.addConstr(d3tot, GRB.GREATER_EQUAL, 1, "C4_x"+i+u);
 				}
 			}
 			
-			// 5 (works)
+			// Constraint 5 (works)
 			for(int i=0; i<amountTeams;++i){
 				for(int u=0; u<n; ++u) {
 					for(int s=0; s<amountSlots-n1; ++s) {
 						GRBLinExpr d4tot = new GRBLinExpr();
 						for(int c=s; c<=s+n1;++c) {
-							System.out.println("i: "+i+"  c: "+c+" u: "+u);
+							//System.out.println("i: "+i+"  c: "+c+" u: "+u);
 							d4tot.addTerm(1.0,x[i][c][u]);
 						}
-						System.out.println("-----------");
-						model.addConstr(d4tot, GRB.LESS_EQUAL, 1, "B4_x"+i+s+u);
+						//System.out.println("-----------");
+						model.addConstr(d4tot, GRB.LESS_EQUAL, 1, "C5_x"+i+s+u);
 					}
 				}
 			}
 			
-			// 6 (works)
+			// Constraint 6 (works)
 			for(int i=0; i<amountTeams;++i){
 				for(int u=0; u<n; ++u) {
 					for(int s=0; s<amountSlots-n2; ++s) {
@@ -149,20 +176,20 @@ public class TUP {
 						for(int c=s; c<=s+n2;++c) {
 							d5tot.addTerm(1.0,x[i][c][u]);
 							for(int j=0; j<amountTeams; ++j) {
-								System.out.println("i: "+i+" u: "+u+" s: "+s+" c: "+c+" j: "+j);
+								//System.out.println("i: "+i+" u: "+u+" s: "+s+" c: "+c+" j: "+j);
 								if(opp[c][j] == i+1) {
-									System.out.println("team "+(j+1)+" speelt op slot "+(c+1)+" tegen "+i);
+									//System.out.println("team "+(j+1)+" speelt op slot "+(c+1)+" tegen "+i);
 									d5tot.addTerm(1.0,x[j][c][u]);
 								}
 							}
 						}
-						System.out.println("-----------");
-						model.addConstr(d5tot, GRB.LESS_EQUAL, 1, "B5_x"+i+s+u);
+						//System.out.println("-----------");
+						model.addConstr(d5tot, GRB.LESS_EQUAL, 1, "C6_x"+i+s+u);
 					}
 				}
 			}
 			
-			// 7
+			// Constraint 7 (works)
 			for(int i=0; i<amountTeams; ++i) {
 				for(int j=0; j<amountTeams; ++j) {
 					for(int u=0; u<n; ++u) {
@@ -171,17 +198,111 @@ public class TUP {
 							d6tot.addTerm(1.0, x[i][s][u]);
 							d6tot.addTerm(1.0, x[j][s+1][u]);
 							d6tot.addTerm(-1.0, z[i][j][s][u]);
-							model.addConstr(d6tot, GRB.LESS_EQUAL, 1, "xisc6"+i+j+u+s);
+							model.addConstr(d6tot, GRB.LESS_EQUAL, 1, "C7"+i+j+u+s);
 						}
 					}
 				}
+			}
+			
+			// Constraint 10
+			for(int i=0; i<amountTeams; i++) {
+				for(int u=0; u<n; u++) {
+					for(int s=0; s<amountSlots; s++) {
+						if(opp[s][i] < 0) {
+							GRBLinExpr d10tot = new GRBLinExpr();
+							d10tot.addTerm(1.0, x[i][s][u]);
+							model.addConstr(d10tot, GRB.EQUAL, 0, "C10_x"+i+s+u);
+						}
+					}
+				}
+			}
+			
+			// Constraint 11
+			for(int i=0; i<amountTeams; i++) {
+				for(int j=0; j<amountTeams; j++) {
+					for(int u=0; u<n; u++) {
+						for(int s=0; s<amountSlots-1; s++) {
+							GRBLinExpr d11tot = new GRBLinExpr();
+							d11tot.addTerm(1.0, z[i][j][s][u]);
+							d11tot.addTerm(-1.0, x[i][s][u]);
+							model.addConstr(d11tot, GRB.LESS_EQUAL, 0, "C11_x"+i+j+u+s);
+						}
+					}
+				}
+			}
+			
+			// Constraint 12
+			for(int i=0; i<amountTeams; i++) {
+				for(int j=0; j<amountTeams; j++) {
+					for(int u=0; u<n; u++) {
+						for(int s=0; s<amountSlots-1; s++) {
+							GRBLinExpr d12tot = new GRBLinExpr();
+							d12tot.addTerm(1.0, z[i][j][s][u]);
+							d12tot.addTerm(-1.0, x[j][s+1][u]);
+							model.addConstr(d12tot, GRB.LESS_EQUAL, 0, "C12_x"+i+j+u+s);
+						}
+					}
+				}
+			}
+			
+			// Constraint 13
+			for(int i=0; i<amountTeams; i++) {
+				for(int u=0; u<n; u++) {
+					for(int s=0; s<amountSlots-2; s++) {
+						GRBLinExpr d13tot = new GRBLinExpr();
+						for(int j=0; j<amountTeams; j++) {
+							d13tot.addTerm(1.0, z[j][i][s][u]);
+						}
+						for(int j=0; j<amountTeams; j++) {
+							d13tot.addTerm(-1.0, z[i][j][s+1][u]);
+						}
+						model.addConstr(d13tot, GRB.EQUAL, 0, "C13_x"+i+u+s);
+					}
+				}
+			}
+			
+			// Constraint 14
+			for(int u=0; u<n; u++) {
+				for(int s=0; s<amountSlots-1; s++) {
+					GRBLinExpr d14tot = new GRBLinExpr();
+					for(int i=0; i<amountTeams; i++) {
+						for(int j=0; j<amountTeams; j++) {
+							d14tot.addTerm(1.0, z[i][j][s][u]);
+						}
+					}
+					model.addConstr(d14tot, GRB.EQUAL, 1, "C14_x"+u+s);
+				}
+			}
+			
+			/*
+			 * Constraints stronger formulation
+			 */
+			
+			// Constraint 15
+			Random rand = new Random();
+			int k = rand.nextInt((int) ((4*n-2-1)+1));
+
+			ArrayList<int[]> venues = new ArrayList<int[]>();
+			int umpire = 0;
+			for(int i=0; i<amountTeams; i++) {
+				if(opp[k][i] > 0) {
+					int[] venue = {i,umpire};
+					umpire++;
+					venues.add(venue);
+				}
+			}
+			
+			for(int[] v: venues) {
+				GRBLinExpr d15tot = new GRBLinExpr();
+				d15tot.addTerm(1.0, x[v[0]][k][v[1]]);
 			}
 			
 			// Solve
 			model.optimize();
 			printSolution(model,x,z);
 
-			
+			model.dispose();
+			env.dispose();
 		} catch (GRBException e) {
 			System.out.println("Error code: " + e.getErrorCode() + ". " +
 								e.getMessage());
@@ -206,8 +327,8 @@ public class TUP {
 //					}
 //				}
 //			}
-			
-			// Print waardes voor alle z
+//			
+//			// Print waardes voor alle z
 //			System.out.println("\nZ:");
 //			for (int i = 0; i < z.length; ++i) {
 //				for(int j = 0; j< z[0].length; ++j) {
@@ -222,6 +343,6 @@ public class TUP {
 		} else {
 			System.out.println("No solution");
 		}
-		}		
+		}	
 	}
 
